f57e58a5180facd7bca8c6138812df72c3e76ce7
treyarnold <arnold.trey80@gmail.com>
2019-05-17 23:24:54 -0500

    manager view created

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Treys-Hardware
 a CLI app simulating a store front.  This App uses Node and MySQL.
+
+>Interface is set with inquirer to make the UI more user friendly
+
+>You can purchase as a customer, or go into manager view to update inventory
+
+[![IMAGE ALT TEXT](./hardwareApp.png)](http://www.youtube.com/watch?v=QyDmL1RcrjA "Hardware Store App")
--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const introPrompt = [
   {
     type: "list",
     message: "Welcome to Trey's Hardware\nWhat would you like to do?",
-    choices: ["View items for sale", "Quit"],
+    choices: ["View items for sale", "Quit", ""],
     name: "introChoice"
   }
 ]
@@ -22,7 +22,7 @@ const connection = mysql.createConnection({
 connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  console.log('\033[2J');
+  clearConsole();
   start();
 });
 
@@ -30,22 +30,24 @@ function start() {
   inquirer.prompt(introPrompt).then(response => {
     switch (response.introChoice) {
       case "View items for sale": showInventory(); break;
-      case "Quit": connection.end();
+      case "Quit": connection.end(); break;
+      case "": clearConsole(); managerView();
     }
   })
 }
 
 function backToMenu() {
-  console.log('\033[2J');
+  clearConsole();
   start();
 }
 
 function showInventory() {
-  console.log('\033[2J');
+  clearConsole();
   let longestProductName = 0;
   let longestDepartmentName = 0;
   let startingSpace = "";
   let priceSpace = "";
+  let quantitySpace = "";
   const prompt = [
     {
       type: "list",
@@ -65,7 +67,9 @@ function showInventory() {
       else startingSpace = "";
       if (item.price < 10) priceSpace = " ";
       else priceSpace = "";
-      const itemListing = `${startingSpace}${item.id} - ${item.product_name.padEnd(longestProductName)} | ${item.department_name.padEnd(longestDepartmentName)} | $${priceSpace}${item.price}`;
+      if (item.stock_quantity < 10) quantitySpace = " ";
+      else quantitySpace = "";
+      const itemListing = `${startingSpace}${item.id} - ${item.product_name.padEnd(longestProductName)} | ${item.department_name.padEnd(longestDepartmentName)} | $${priceSpace}${item.price} | Qty: ${quantitySpace}${item.stock_quantity}`;
       prompt[0].choices.push(itemListing);
     });
     prompt[0].choices.push("Go Back");
@@ -74,14 +78,14 @@ function showInventory() {
       else {
         const itemNumber = product.purchased.slice(0, 2).trim();
         const id = parseInt(itemNumber);
-        console.log('\033[2J');
+        clearConsole();
         purchased(id);
       }
     })
   })
 }
 
-function purchased(id) {  
+function purchased(id) {
   connection.query("SELECT * FROM inventory WHERE id = ?", id, (err, res) => {
     if (err) throw err
     if (res[0].stock_quantity <= 0) {
@@ -99,17 +103,17 @@ function purchased(id) {
         const quantityPurchased = parseInt(purchase.quantity);
         if (quantityPurchased === 0) {
           console.log("No purchase made.");
-          backToMenu();
+          start();
         } else if (quantityPurchased <= res[0].stock_quantity) {
-          connection.query("UPDATE inventory SET stock_quantity = ? WHERE id = ?", [res[0].stock_quantity - quantityPurchased, 
+          connection.query("UPDATE inventory SET stock_quantity = ? WHERE id = ?", [res[0].stock_quantity - quantityPurchased,
           res[0].id], (err, response) => {
             if (err) throw err
-            console.log('\033[2J');
+            clearConsole();
             console.log(`Thank you for your purchase of ${quantityPurchased} ${res[0].product_name} for $${res[0].price * quantityPurchased}\n`);
             start();
-            });
+          });
         } else {
-          console.log('\033[2J');
+          clearConsole();
           console.log("I am sorry, but I do not have that many.");
           purchased(id);
         }
@@ -127,4 +131,172 @@ function soldOut() {
     }
   ]
   inquirer.prompt(prompt).then(input => showInventory());
+}
+
+function managerView() {
+  const prompt = [
+    {
+      type: "list",
+      message: "Welcome Manager\nWe are going to pretend you had to login\nWhat would you like to do?",
+      choices: ["View Inventory", "View Inventory that is low", "Add to Inventory", "Add new item to Inventory", "Go Back"],
+      name: "managerChoice"
+    }
+  ];
+  inquirer.prompt(prompt).then(input => {
+    switch (input.managerChoice) {
+      case "View Inventory": viewInventory(); break;
+      case "View Inventory that is low": viewLowInventory(); break;
+      case "Add to Inventory": AddToInventory(); break;
+      case "Add new item to Inventory": console.log("new item;"); start(); break;
+      case "Go Back": clearConsole(); start(); break;
+    }
+  })
+}
+
+function clearConsole() {
+  console.log('\033[2J');
+}
+
+function viewInventory() {
+  let longestProductName = 0;
+  let longestDepartmentName = 0;
+  let startingSpace = "";
+  let priceSpace = "";
+  clearConsole();
+  connection.query("SELECT * FROM inventory", (err, res) => {
+    if (err) throw err
+    res.forEach(item => {
+      if (item.product_name.length > longestProductName) longestProductName = item.product_name.length
+      if (item.department_name.length > longestDepartmentName) longestDepartmentName = item.department_name.length
+    })
+    res.forEach(item => {
+      if (item.id < 10) startingSpace = " ";
+      else startingSpace = "";
+      if (item.price < 10) priceSpace = " ";
+      else priceSpace = "";
+      if (item.stock_quantity < 10) quantitySpace = " ";
+      else quantitySpace = "";
+      console.log(`${startingSpace}${item.id} - ${item.product_name.padEnd(longestProductName)} | ${item.department_name.padEnd(longestDepartmentName)} | $${priceSpace}${item.price} | Qty: ${quantitySpace}${item.stock_quantity}`);
+    });
+    inquirer.prompt([
+      {
+        type: "input",
+        message: "Press Enter to return to the Manager Menu",
+        name: "pause"
+      }
+    ]).then(input => {
+      clearConsole();
+      managerView();
+    });
+  });
+}
+
+function viewLowInventory() {
+  let longestProductName = 0;
+  let longestDepartmentName = 0;
+  let startingSpace = "";
+  let priceSpace = "";
+  clearConsole();
+  connection.query("SELECT * FROM inventory WHERE stock_quantity < 5", (err, res) => {
+    if (err) throw err
+    res.forEach(item => {
+      if (item.product_name.length > longestProductName) longestProductName = item.product_name.length
+      if (item.department_name.length > longestDepartmentName) longestDepartmentName = item.department_name.length
+    })
+    res.forEach(item => {
+      if (item.id < 10) startingSpace = " ";
+      else startingSpace = "";
+      if (item.price < 10) priceSpace = " ";
+      else priceSpace = "";
+      if (item.stock_quantity < 10) quantitySpace = " ";
+      else quantitySpace = "";
+      console.log(`${startingSpace}${item.id} - ${item.product_name.padEnd(longestProductName)} | ${item.department_name.padEnd(longestDepartmentName)} | $${priceSpace}${item.price} | Qty: ${quantitySpace}${item.stock_quantity}`);
+    });
+    inquirer.prompt([
+      {
+        type: "input",
+        message: "Press Enter to return to the Manager Menu",
+        name: "pause"
+      }
+    ]).then(input => {
+      clearConsole();
+      managerView();
+    });
+  });
+}
+
+function AddToInventory() {
+  clearConsole();
+  let longestProductName = 0;
+  let longestDepartmentName = 0;
+  let startingSpace = "";
+  let priceSpace = "";
+  let quantitySpace = "";
+  const prompt = [
+    {
+      type: "list",
+      message: "Which Item are you going to add stock to?",
+      choices: [],
+      name: "stock"
+    }
+  ]
+  connection.query("SELECT * FROM inventory", (err, res) => {
+    if (err) throw err
+    res.forEach(item => {
+      if (item.product_name.length > longestProductName) longestProductName = item.product_name.length
+      if (item.department_name.length > longestDepartmentName) longestDepartmentName = item.department_name.length
+    })
+    res.forEach(item => {
+      if (item.id < 10) startingSpace = " ";
+      else startingSpace = "";
+      if (item.price < 10) priceSpace = " ";
+      else priceSpace = "";
+      if (item.stock_quantity < 10) quantitySpace = " ";
+      else quantitySpace = "";
+      const itemListing = `${startingSpace}${item.id} - ${item.product_name.padEnd(longestProductName)} | ${item.department_name.padEnd(longestDepartmentName)} | $${priceSpace}${item.price} | Qty: ${quantitySpace}${item.stock_quantity}`;
+      prompt[0].choices.push(itemListing);
+    });
+    prompt[0].choices.push("Go Back");
+    inquirer.prompt(prompt).then(product => {
+      if (product.stock === "Go Back") {
+        clearConsole();
+        managerView();
+      } else {
+        const itemNumber = product.stock.slice(0, 2).trim();
+        const id = parseInt(itemNumber);
+        clearConsole();
+        updateStock(id);
+      }
+    })
+  })
+}
+
+function updateStock(id) {
+  console.log(id);
+  connection.query("SELECT * FROM inventory WHERE id = ?", id, (err, res) => {
+    if (err) throw err    
+    const prompt = [
+      {
+        type: "input",
+        message: `You have selected ${res[0].product_name} from the ${res[0].department_name} department. The current stock is ${res[0].stock_quantity}.\nHow Many would you like to add?`,
+        name: "quantity"
+      }
+    ]
+    inquirer.prompt(prompt).then(inventory => {
+      const quantityAdded = parseInt(inventory.quantity);
+      if (quantityAdded === 0) {
+        clearConsole();
+        console.log("No new stock added.");
+        managerView();
+      } else {
+        const newStockQuantity = res[0].stock_quantity + quantityAdded;
+        connection.query("UPDATE inventory SET stock_quantity = ? WHERE id = ?", [newStockQuantity, res[0].id], (err, response) => {
+          if (err) throw err
+          clearConsole();
+          console.log(`You have added ${quantityAdded}  to ${res[0].product_name} bringing the new stock available to ${newStockQuantity}\n`);
+          managerView();
+        });
+      } 
+    });
+  })
 }
